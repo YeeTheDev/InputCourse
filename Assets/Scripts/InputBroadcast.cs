3331e6954e160670c76a73637b81c7d13406641c
Yee <yee.devindie@gmail.com>
2025-04-02 13:36:12 -0600

    L34 - Broadcast messages

---
+++ Assets/Scripts/InputBroadcast.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputBroadcast : MonoBehaviour
+{
+    private void OnExample()
+    {
+        Debug.Log("Positive reviews really help the course");
+    }
+}
